9f904ec3481840e77176db29de0d4482ce948be8
mattia-gallinaro <72882041+mattia-gallinaro@users.noreply.github.com>
2021-10-21 19:27:41 +0200

    Continuazione Analisi Tecnica e Inizio Codice

---
+++ Fantacalcio/Fantacalcio/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Fantacalcio
+{
+    class Calciatore
+    {
+        string nome_e_cognome;
+        string ruolo;
+        string sqaudra;
+        int costo;
+        int PunteggioPartita;
+        public List<Calciatore> GetCalciatori(string[] array)
+        {
+            List<Calciatore> calciatorifile = new List<Calciatore>();
+
+            return calciatorifile;
+        }
+
+    }
+    class Giocatori
+    {
+        string nome;
+        int punteggio;
+        int crediti;
+        List<Calciatore> Rosa = new List<Calciatore>();
+        List<Calciatore> Formazione = new List<Calciatore>();
+        public void ControlloNomeInserito()
+        {
+
+        }
+        public void Asta()
+        {
+
+        }
+        public void AssegnazioneRosa()
+        {
+
+        }
+        public void AssegnazioneFormazione()
+        {
+
+        }
+    }
+    
+    class Partita
+    {
+        public void GenerazionePartita()
+        {
+
+        }
+        public void AssegnazionePunteggioCalciatore()
+        {
+
+        }
+        public void DichiarazioneVincitore()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] calciatorifile;
+            string filecalciatoripath = AppDomain.CurrentDomain.BaseDirectory + "Calciatori.txt";
+            calciatorifile = File.ReadAllLines(filecalciatoripath);
+            List<Calciatore> calciatori = new List<Calciatore>();
+            Calciatore c = new Calciatore();
+            calciatori = c.GetCalciatori(calciatorifile);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
