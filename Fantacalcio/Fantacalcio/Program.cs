63a4150a7a2749e6a7b717ca29a3368b43b7ea04
mattia-gallinaro <72882041+mattia-gallinaro@users.noreply.github.com>
2021-10-25 20:37:43 +0200

    Lista calciatori

--- Fantacalcio/Fantacalcio/Program.cs
+++ Fantacalcio/Fantacalcio/Program.cs
@@ -14,7 +14,17 @@ namespace Fantacalcio
         public List<Calciatore> GetCalciatori(string[] array)
         {
             List<Calciatore> calciatorifile = new List<Calciatore>();
-
+            for (int i = 0; i < array.Length; i++)
+            {
+                string[] arraycalciatore = array[i].Split(',');
+                calciatorifile.Add(new Calciatore());
+                calciatorifile[i].nome_e_cognome = arraycalciatore[0];
+                calciatorifile[i].ruolo = arraycalciatore[1];
+                calciatorifile[i].sqaudra = arraycalciatore[2];
+                calciatorifile[i].PunteggioPartita = 0;
+                calciatorifile[i].costo = 0;
+            }
+            
             return calciatorifile;
         }
 
@@ -69,6 +79,21 @@ namespace Fantacalcio
             List<Calciatore> calciatori = new List<Calciatore>();
             Calciatore c = new Calciatore();
             calciatori = c.GetCalciatori(calciatorifile);
+            List<Giocatori> giocatoris = new List<Giocatori>();
+            Console.WriteLine("Premi a se vuoi iniziare un nuovo campionato \n b per continuare un campionato gia' iniziato");
+            string risposta = Console.ReadLine();
+            switch (risposta)
+            {
+                case "a":
+                    break;
+                case "b":
+                    break;
+            }
+            //chiedo all'utente quanti giocatori devono essere inseriti ed il loro nome
+            Console.WriteLine("Premi a se vuoi iniziare un nuovo campionato \n b per continuare un campionato gia' iniziato");
+            risposta = Console.ReadLine();
+            //chiedo all'utente il calciatore da mettere all'asta
+            //chiedo all'utente le formazioni di ogni giocatore
             Console.WriteLine("Hello World!");
         }
     }
