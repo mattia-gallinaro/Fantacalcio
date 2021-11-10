bd086188f5f0d3711eafb90be75c5e0bff96dad2
mattia-gallinaro <72882041+mattia-gallinaro@users.noreply.github.com>
2021-11-10 19:38:08 +0100

    Funzione Asta

--- Fantacalcio-main/Fantacalcio/Fantacalcio/Program.cs
+++ Fantacalcio-main/Fantacalcio/Fantacalcio/Program.cs
@@ -12,12 +12,9 @@ namespace Fantacalcio
         public int costo { get; set; }
         int PunteggioPartita;
         bool gia_assegnato;
-        //public Calciatore(string nome_e_cognome, string ruolo, string sqaudra)
-        //{
-        //    this.nome_e_cognome = nome_e_cognome;
-        //    this.ruolo = ruolo;
-        //    this.sqaudra = sqaudra;
-        //}
+        public Calciatore()
+        {
+        }
         public List<Calciatore> GetCalciatori(string[] array)
         {
             List<Calciatore> calciatorifile = new List<Calciatore>();
@@ -30,23 +27,43 @@ namespace Fantacalcio
                 calciatorifile[i].sqaudra = arraycalciatore[2];
                 calciatorifile[i].PunteggioPartita = 0;
                 calciatorifile[i].costo = 0;
+                calciatorifile[i].gia_assegnato = false;
             }
             
             return calciatorifile;
         }
-
+        public bool ControlloCalciatoreAsta(List<Calciatore> calciatori, string risposta)
+        {
+            bool controllo = false;
+            for(int i = 0; i < calciatori.Count && !controllo; i++)
+            {
+                if(calciatori[i].nome_e_cognome == risposta)
+                {
+                    if (calciatori[i].gia_assegnato)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        controllo = true;
+                    }
+                }
+            }
+            return controllo;
+        }
+            
     }
     class Giocatori
     {
         public string nome { get; set; }
-        int punteggio;
-        int crediti;
+        //int punteggio;
+        public int crediti { get; set; }
         List<Calciatore> Rosa = new List<Calciatore>();
         List<Calciatore> Formazione = new List<Calciatore>();
         public bool ControlloNomeInserito(string risposta, string[] nomigiocatori)
         {
             bool verifica = false;
-            if(risposta == "")
+            if(risposta == "" || risposta.Contains(","))
             {
                 verifica = true;
                 return verifica;
@@ -67,11 +84,71 @@ namespace Fantacalcio
                 return verifica;
             }
         }
-        public void Asta()
+        public int ControlloPrezzoInserito()
         {
-
+            try
+            {
+                int valore = int.Parse(Console.ReadLine());
+                if(valore < 0 || valore > this.crediti)
+                {
+                    return 111;
+                }
+                else
+                {
+                    return valore;
+                }     
+            }
+            catch
+            {
+                return 111;
+            }
         }
-        public void AssegnazioneRosa()
+        public int Asta(int[] prezzi)
+        {
+            int[] indici = new int[0];
+            int max = -1;
+            for(int i = 0; i< prezzi.Length; i++)
+            {
+                if(max < prezzi[i])
+                {
+                    max = prezzi[i];
+                    Console.WriteLine($"{prezzi[i]}");
+                }        
+            }
+            int m = 0;
+            for(int j = 0; j < prezzi.Length; j++)
+            {
+                if(prezzi[j] == max)
+                {
+                    Array.Resize(ref indici, indici.Length + 1);
+                    indici[m] = j;
+                    m++;
+                }
+            }
+            if(indici.Length == 1)
+            {
+                return indici[0];
+            }
+            else
+            {
+                Random random = new Random();
+                int numero_generato = random.Next(0, indici.Length);
+                return indici[numero_generato];
+            }
+        }
+        public bool ControlloRose(ref List<Giocatori> giocatoris)
+        {
+            bool controllo = false;
+            for (int i = 0; i < giocatoris.Count; i++)
+            {
+                if (giocatoris[i].Rosa.Count < 11)
+                {
+                    controllo = true;
+                }
+            }
+            return controllo;
+        }
+        public void AssegnazioneRosa(ref List<Calciatore> calciatores, int indice_c)
         {
 
         }
@@ -149,7 +226,36 @@ namespace Fantacalcio
                 giocatori.Add(new Giocatori());
                 giocatori[i].nome = nomigiocatori[i];
             }
+            int[] prezzo = new int[giocatoris.Count];
+            int indice;
             //chiedo all'utente il calciatore da mettere all'asta
+            do
+            {
+                risposta = "";
+                Console.WriteLine("Inserisci il nome del calciatore da mettere all'asta (es mauro juan)");
+                risposta = Console.ReadLine();
+                while (c.ControlloCalciatoreAsta(calciatori, risposta))
+                {
+                    Console.WriteLine("Il giocatore inserito non esiste oppure e' gia' stato assegnato\n Inserisci un'altro giocatore");
+                    risposta = Console.ReadLine();
+                }
+                int i;
+                for(i = 0; i < giocatoris.Count; i++)
+                {
+                    do
+                    {
+                        Console.WriteLine($"Giocatore {giocatoris[i].nome} inserisci il prezzo per l'asta per il calciatore");
+                        prezzo[i] = giocatoris[i].ControlloPrezzoInserito();
+                    } while (prezzo[i] > 110);
+                }
+                indice = g.Asta(prezzo);
+                giocatoris[i].AssegnazioneRosa(ref calciatori, indice);
+                if (!g.ControlloRose(ref giocatoris))
+                {
+                    Console.WriteLine("Vuoi terminare l'asta?\n Se si allora scrivi yes \n in caso tu voglia continuare l'asta ");
+                }
+            } while (risposta != "Yes" && risposta != "Y" && risposta != "yes" && risposta!= "y");
+            
             //chiedo all'utente le formazioni di ogni giocatore
             Console.WriteLine("Hello World!");
         }
@@ -158,7 +264,7 @@ namespace Fantacalcio
             try
             {
                 int numero = int.Parse(risposta);
-                if(numero < 2 || numero > 10)
+                if (numero < 2 || numero > 10)
                 {
                     return false;
                 }
